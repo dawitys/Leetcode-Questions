975553797498f1a0648b5aa1b081b3d7bb0c783d
Dawit Yonas <dawityonas010@gmail.com>
2022-07-15 08:58:35 +0300

    Time: 228 ms (44.45%), Space: 17.9 MB (17.31%) - LeetHub

---
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        def inBound(x,y):
+            return 0<= x<len(grid) and 0<=y<len(grid[0])
+        
+        DIRS = [[1,0],[0,1],[-1,0],[0,-1]]
+        visited = set()
+        maxArea = 0
+        
+        def dfs(x,y):
+            if (x,y) in visited:
+                return 0
+            visited.add((x,y))
+            area = 1
+            for dx,dy in DIRS:
+                nx,ny = x+dx,y+dy
+                if inBound(nx,ny) and grid[nx][ny]==1:
+                    area += dfs(nx,ny)
+            return area
+        
+        for i in range(len(grid)):
+            for j in range( len(grid[0])):
+                if grid[i][j]==1:
+                    maxArea = max(maxArea,dfs(i,j))
+        
+        return maxArea
