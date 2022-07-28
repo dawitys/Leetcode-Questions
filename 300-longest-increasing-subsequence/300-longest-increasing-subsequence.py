7f0bf6913b6b9582cba1a695311acc6fe2f1ca54
Dawit Yonas <dawityonas010@gmail.com>
2022-07-28 17:01:06 +0300

    Time: 118 ms (83.33%), Space: 14.2 MB (46.59%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -5,18 +5,16 @@ class Solution:
         # arr = [1,2,3,4,5,6,6,6,6,6,7]        
         # return bisect.bisect_right(arr,a)
         
-        ans = 1
-        dp = [1]*len(nums)
+        # 
         
-        for i in range(len(nums)-1,-1,-1):
-            maxpossible = 1
-            for j in range(i+1,len(nums)):
-                if nums[j] > nums[i]:
-                    maxpossible = max(maxpossible,dp[j]+1)
-            
-            
-            dp[i] = maxpossible
-            
-        # print(dp)
-        return max(dp)
+        ans = 1
+        dp = []
+        
+        for n in nums:
+            pos =bisect.bisect_left(dp,n)
+            if pos == len(dp):
+                dp.append(n)
+            else:
+                dp[pos] = n
+        return len(dp)
         
