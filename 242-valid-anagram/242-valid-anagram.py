010fddde43346631ab3ce971ddf544781cb44732
Dawit Yonas <dawityonas010@gmail.com>
2022-07-28 10:13:35 +0300

    Time: 78 ms (48.72%), Space: 14.4 MB (96.92%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,14 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        counter1 = Counter(s)
-        counter2 = Counter(t)
-        
-        for c in counter1:
-            if c not in counter2 or counter2[c] != counter1[c]:
-                return False
-            
-        for c in counter2:
-            if c not in counter1 or counter2[c] != counter1[c]:
-                return False
-            
-        return True
+        letters = [0] * 27
+        for i, j in zip_longest(s, t, fillvalue='{'):
+            letters[ord(i) - 97] += 1
+            letters[ord(j) - 97] -= 1
+        return any(letters) is False
