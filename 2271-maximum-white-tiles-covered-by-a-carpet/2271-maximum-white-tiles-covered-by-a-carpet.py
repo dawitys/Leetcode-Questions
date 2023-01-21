a64b4df0ae0676df10c4ee8fea77bde0cf5d4154
Dawit Yonas <dawityonas010@gmail.com>
2023-01-21 10:16:05 +0300

    Time: 1387 ms (60.57%), Space: 39.1 MB (93.14%) - LeetHub

--- 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.py
+++ 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.py
@@ -1,12 +1,10 @@
 class Solution:
     def maximumWhiteTiles(self, tiles: List[List[int]], carpetLen: int) -> int:
         tiles.sort()
-        # build the starting position array
-        startPos = [tiles[i][0] for i in range(len(tiles))]
-        # build the prefix sum array
-        preSum = [0] * (len(tiles) + 1)
+        
+        prefix = [0] * (len(tiles) + 1)
         for i in range(1, len(tiles) + 1):
-            preSum[i] = preSum[i - 1] + (tiles[i-1][1]-tiles[i-1][0] + 1)
+            prefix[i] = prefix[i - 1] + (tiles[i-1][1]-tiles[i-1][0] + 1)
         
         res = 0
         for i in range(len(tiles)):
@@ -15,14 +13,14 @@ class Solution:
             if e >= s + carpetLen - 1:
                 return carpetLen
             # binary search the index of the ending tile that the carpet can partially cover
-            endIdx = bisect_right(startPos, s + carpetLen - 1) - 1
+            endIdx = bisect_right(tiles, s + carpetLen - 1,key=lambda tile:tile[0]) - 1
             # calculate the length of the ending tile that the carpet cannot cover 
             compensate = 0
             if tiles[endIdx][1] > s + carpetLen - 1:
                 compensate = tiles[endIdx][1] - s - carpetLen + 1
                 
             # update the result
-            res = max(res, preSum[endIdx+1] - preSum[i] - compensate)
+            res = max(res, prefix[endIdx+1] - prefix[i] - compensate)
             
         return res
         
