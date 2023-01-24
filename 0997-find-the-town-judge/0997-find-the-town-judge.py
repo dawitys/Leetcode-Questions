3a734770b0fc150a7ae7b5e0e6c4a9787bb543ec
Dawit Yonas <dawityonas010@gmail.com>
2023-01-24 13:46:27 +0300

    Time: 938 ms (40.29%), Space: 18.9 MB (58.92%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -5,10 +5,10 @@ class Solution:
         
         for s,e in trust:
             graph[e-1] += 1
-            non_judges.add(s-1)
+            graph[s-1] -= 1
             
         for i in range(n):
-            if graph[i] == n-1 and i not in non_judges:
+            if graph[i] == n-1:
                 return i+1
             
         return -1
