427d0c7bca4d51e45ab93e60fcc687d11d29dc4e
Dawit Yonas <dawityonas010@gmail.com>
2023-02-03 14:16:25 +0300

    Time: 39 ms (60.05%), Space: 14.1 MB (65.72%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
@@ -8,13 +8,18 @@ class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
         levels = defaultdict(list)
         min_level, max_level = float("inf"), -float("inf")
-        def dfs(root, level, order):
+        def dfs(node, level, order):
+            if not node:
+                return
+            
             nonlocal min_level, max_level
             min_level = min(level, min_level)
             max_level = max(level, max_level)
-            levels[level].append((order, root.val))
-            if root.left:  dfs(root.left,  level-1, order+1)
-            if root.right: dfs(root.right, level+1, order+1)
+            
+            levels[level].append((order, node.val))
+            
+            dfs(node.left,  level-1, order+1)
+            dfs(node.right, level+1, order+1)
         
         dfs(root, 0, 0)
         return [[j for i,j in sorted(levels[i])] for i in range(min_level,max_level+1)]
