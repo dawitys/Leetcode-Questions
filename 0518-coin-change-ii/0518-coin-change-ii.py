dc2ddef8449997237eecc8821f30c88439bfd3c7
Dawit Yonas <dawityonas010@gmail.com>
2023-02-13 14:44:14 +0300

    Time: 200 ms (73.69%), Space: 14.1 MB (62.50%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,18 +1,10 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        cache = {}
-        def dfs(rem,idx,cache):
-            if (rem,idx) in cache:
-                return cache[(rem,idx)]
-            if rem < 0:
-                return 0
-            if rem == 0:
-                return 1
-            ways = 0
-            for i in range(idx, len(coins)):
-                if rem >= coins[i]:
-                    ways += dfs(rem-coins[i],i,cache)
-                    
-            cache[(rem,idx)] = ways
-            return ways
-        return dfs(amount,0,cache)
+        dp = [0] * (amount+1)
+        dp[0] = 1
+        for c in coins:
+            for curr in range(1,amount+1):
+                if curr >= c:
+                    dp[curr] += dp[curr-c]
+
+        return dp[amount]
