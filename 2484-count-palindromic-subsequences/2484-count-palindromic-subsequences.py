4cdc577794c2ddcf07ffe377e5d6f72e51f88141
Dawit Yonas <dawityonas010@gmail.com>
2023-06-16 01:22:44 +0300

    Time: 2785 ms (5.08%), Space: 54.8 MB (15.25%) - LeetHub

--- 2484-count-palindromic-subsequences/2484-count-palindromic-subsequences.py
+++ 2484-count-palindromic-subsequences/2484-count-palindromic-subsequences.py
@@ -1,53 +1,37 @@
 class Solution:
     def countPalindromes(self, s: str) -> int:
-        '''
-        prefix = [[0]*10 for _ in range(len(s))]
-        suffix = [[0]*10 for _ in range(len(s))]
-        
-        prefix[0][int(s[0])] = 1
-        suffix[-1][int(s[-1])] = 1
+       
+        n = len(s)
+        prefix = [[[0]*10 for _ in range(10)] for _ in range(n)]
+        suffix = [[[0]*10 for _ in range(10)] for _ in range(n)]
 
-            
-        for i in range(1,len(s)):
-            for j in range(10):
-                prefix[i][j] = prefix[i-1][j] + int(int(s[i]) == j)
-        for i in range(len(s)-2,-1,-1):
-            for j in range(10):
-                suffix[i][j] = suffix[i+1][j] + int(int(s[i]) == j)
-                
-        total = 0
-        MOD = 10**9 + 7
-        print(prefix,suffix,)
-        for i in range(2,len(s)-2):
-            pals = 0
-            for j in range(10):
-                for k in range(10):
-                    pals += min(prefix[i-1][j],suffix[i+1][j]) * min(prefix[i-2][k],suffix[i+2][k])
-            total += pals
-        return total % MOD
-        '''
-        mod, n, ans = 10 ** 9 + 7, len(s), 0
-        pre, cnts = [[[0] * 10 for _ in range(10)] for _ in range(n)], [0] * 10
+        counts = [0]*10
         for i in range(n):
-            c = ord(s[i]) - ord('0')
             if i:
                 for j in range(10):
                     for k in range(10):
-                        pre[i][j][k] = pre[i - 1][j][k] 
-                        if k == c: pre[i][j][k] += cnts[j]
-            cnts[c] += 1
-        suf, cnts = [[[0] * 10 for _ in range(10)] for _ in range(n)], [0] * 10
-        for i in range(n - 1, -1, -1):
-            c = ord(s[i]) - ord('0')
+                        prefix[i][j][k] = prefix[i-1][j][k]
+                        if int(s[i]) == k:
+                            prefix[i][j][k] += counts[j]
+                        
+            counts[int(s[i])] += 1
+        
+        counts = [0]*10          
+        for i in range(n-1,-1,-1):
             if i < n - 1:
                 for j in range(10):
                     for k in range(10):
-                        suf[i][j][k] = suf[i + 1][j][k]
-                        if k == c: suf[i][j][k] += cnts[j]
-            cnts[c] += 1
-        for i in range(2, n - 2):
+                        suffix[i][j][k] = suffix[i+1][j][k] 
+                        if int(s[i]) == k:
+                            suffix[i][j][k] += counts[j]
+
+            counts[int(s[i])] += 1
+        total = 0
+        MOD = 10**9 + 7
+
+        for i in range(2,n-2):
             for j in range(10):
                 for k in range(10):
-                    ans += pre[i - 1][j][k] * suf[i + 1][j][k]
-        return ans % mod
-    
+                    total += prefix[i-1][j][k]*suffix[i+1][j][k]
+
+        return total % MOD
