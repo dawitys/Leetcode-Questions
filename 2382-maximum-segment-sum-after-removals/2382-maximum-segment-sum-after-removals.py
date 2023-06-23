f2185f527c7ec8e8180054ad39cf3664fb75e56e
Dawit Yonas <dawityonas010@gmail.com>
2023-06-23 23:55:15 +0300

    Time: 1440 ms (51.43%), Space: 98.6 MB (7.86%) - LeetHub

---
+++ 2382-maximum-segment-sum-after-removals/2382-maximum-segment-sum-after-removals.py
@@ -0,0 +1,39 @@
+class UnionFind:
+    def __init__(self):
+        self.parent = {}
+        self.sum = {}
+        self.maxSumSegment = 0
+        
+    def addAndMergeAdjSegment(self, u, value):
+        self.parent[u] = u
+        self.sum[u] = value
+        self.maxSumSegment = max(self.maxSumSegment, value)
+        if u-1 in self.parent:
+            self.union(u, u-1)
+        if u+1 in self.parent:
+            self.union(u, u+1)
+        
+    def find(self, u):
+        if u != self.parent[u]:
+            self.parent[u] = self.find(self.parent[u])  # Path compression
+        return self.parent[u]
+    
+    def union(self, u, v):
+        pu = self.find(u)
+        pv = self.find(v)
+        if pu == pv: return
+        self.sum[pu] += self.sum[pv]
+        self.parent[pv] = pu
+        self.maxSumSegment = max(self.maxSumSegment, self.sum[pu])
+
+class Solution:
+    def maximumSegmentSum(self, nums: List[int], removeQueries: List[int]) -> List[int]:
+        n = len(nums)
+        uf = UnionFind()
+        
+        ans = [0] * n
+        for i in range(n-1, -1, -1):
+            ans[i] = uf.maxSumSegment
+            q = removeQueries[i]
+            uf.addAndMergeAdjSegment(q, nums[q])
+        return ans
