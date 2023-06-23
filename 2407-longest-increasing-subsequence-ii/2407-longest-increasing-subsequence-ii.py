62177fbb752c4764d42d2d45a870e1ee6aff4f7d
Dawit Yonas <dawityonas010@gmail.com>
2023-06-24 02:03:58 +0300

    Time: 2560 ms (64.86%), Space: 26.5 MB (56.76%) - LeetHub

--- 2407-longest-increasing-subsequence-ii/2407-longest-increasing-subsequence-ii.py
+++ 2407-longest-increasing-subsequence-ii/2407-longest-increasing-subsequence-ii.py
@@ -1,82 +1,41 @@
+class SegTree: 
+
+    def __init__(self, arr: List[int]): 
+        self.n = len(arr)
+        self.tree = [0] * (2*self.n)
+        # for i in range(2*self.n-1, 0, -1): 
+        #     if i >= self.n: self.tree[i] = arr[i - self.n]
+        #     else: self.tree[i] = max(self.tree[i<<1], self.tree[i<<1|1])
+
+    def query(self, lo: int, hi: int) -> int: 
+        ans = 0 
+        lo += self.n 
+        hi += self.n
+        while lo < hi: 
+            if lo & 1: 
+                ans = max(ans, self.tree[lo])
+                lo += 1
+            if hi & 1: 
+                hi -= 1
+                ans = max(ans, self.tree[hi])
+            lo >>= 1
+            hi >>= 1
+        return ans 
+
+    def update(self, i: int, val: int) -> None: 
+        i += self.n 
+        self.tree[i] = val
+        while i > 1: 
+            self.tree[i>>1] = max(self.tree[i], self.tree[i^1])
+            i >>= 1
 
 class Solution:
     def lengthOfLIS(self, nums: List[int], k: int) -> int:
-        
-        ml=[[] for i in range(6)]
-        ml[0]=1
-        ml[1]=[0]*11
-        ml[2]=[0]*101
-        ml[3]=[0]*1001
-        ml[4]=[0]*10001
-        ml[5]=[0]*100001
-        
-        n=nums[0]
-        ml[5][n]=1
-        ml[4][n//10]=1
-        ml[3][n//100]=1
-        ml[2][n//1000]=1
-        ml[1][n//10000]=1
-        for n in nums[1:]:
-            pos1=max(0, n-k)
-            mx=0
-            for i in range((pos1-1)//10000+1, n//10000):
-                if ml[1][i]>mx:
-                    mx=ml[1][i]
-            if k//10000>1:
-                for i in range((pos1-1)//1000+1, ((pos1-1)//10000+1)*10):
-                    if ml[2][i]>mx:
-                        mx=ml[2][i]
-                for i in range((n//10000)*10, n//1000):
-                    if ml[2][i]>mx:
-                        mx=ml[2][i]
-            else:
-                for i in range((pos1-1)//1000+1, n//1000):
-                    if ml[2][i]>mx:
-                        mx=ml[2][i]
-            if k//1000>1:
-                for i in range((pos1-1)//100+1, ((pos1-1)//1000+1)*10):
-                    if ml[3][i]>mx:
-                        mx=ml[3][i]
-                for i in range((n//1000)*10, n//100):
-                    if ml[3][i]>mx:
-                        mx=ml[3][i]
-            else:
-                for i in range((pos1-1)//100+1, n//100):
-                    if ml[3][i]>mx:
-                        mx=ml[3][i]
-            if k//100>1:
-                for i in range((pos1-1)//10+1, ((pos1-1)//100+1)*10):
-                    if ml[4][i]>mx:
-                        mx=ml[4][i]
-                for i in range((n//100)*10, n//10):
-                    if ml[4][i]>mx:
-                        mx=ml[4][i]
-            else:
-                for i in range((pos1-1)//10+1, n//10):
-                    if ml[4][i]>mx:
-                        mx=ml[4][i]
-            if k//10>1:
-                for i in range(pos1, ((pos1-1)//10+1)*10):
-                    if ml[5][i]>mx:
-                        mx=ml[5][i]
-                for i in range((n//10)*10, n):
-                    if ml[5][i]>mx:
-                        mx=ml[5][i]
-            else:
-                for i in range(pos1, n):
-                    if ml[5][i]>mx:
-                        mx=ml[5][i]
-            mx+=1
-            if mx>ml[5][n]:
-                ml[5][n]=mx
-            if mx>ml[4][n//10]:
-                ml[4][n//10]=mx
-            if mx>ml[3][n//100]:
-                ml[3][n//100]=mx
-            if mx>ml[2][n//1000]:
-                ml[2][n//1000]=mx
-            if mx>ml[1][n//10000]:
-                ml[1][n//10000]=mx
-            if mx>ml[0]:
-                ml[0]=mx
-        return ml[0]
+        m = max(nums)
+        ans = 0 
+        tree = SegTree([0] * (m+1))
+        for x in nums: 
+            val = tree.query(max(0, x-k), x) + 1
+            ans = max(ans, val)
+            tree.update(x, val)
+        return ans 
