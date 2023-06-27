c90d94d1f3f2faa152f589325729398b1f1ac4f2
Dawit Yonas <dawityonas010@gmail.com>
2023-06-28 01:29:05 +0300

    Time: 701 ms (17.92%), Space: 17.6 MB (50.94%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.py
+++ 1314-matrix-block-sum/1314-matrix-block-sum.py
@@ -1,14 +1,18 @@
 class Solution:
     def matrixBlockSum(self, mat: List[List[int]], k: int) -> List[List[int]]:
-        answer = [[0 for i in range(len(mat[0]))] for j in range(len(mat))]
-        answer2 = [[0 for i in range(len(mat[0]))] for j in range(len(mat))]
+        dp = mat[::]
+        
+        for i in range(len(mat)):
+            for j in range(1,len(mat[0])):
+                dp[i][j] = dp[i][j-1] + mat[i][j]
+        
+        ans = [[0 for _ in range(len(mat[0]))] for _ in range (len(mat))]
 
         for i in range(len(mat)):
             for j in range(len(mat[0])):
-                answer[i][j] = sum([mat[i][l]  for l in range(max(0,j-k),min(len(mat[0]),j+k+1))]) 
-        
-        for i in range(len(answer[0])):
-            for j in range(len(answer)):
-                answer2[j][i] = sum([answer[l][i]  for l in range(max(0,j-k),min(len(mat),j+k+1))])  
-                
-        return answer2
+                for r in range(max(0,i-k),min(len(mat),i+k+1)):
+                    ans[i][j] += dp[r][min(len(mat[0])-1,j+k)]
+                    if j-k > 0:
+                        ans[i][j] -= dp[r][j-k-1]
+                        
+        return ans
