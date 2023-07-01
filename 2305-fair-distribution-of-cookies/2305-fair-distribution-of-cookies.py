067fd89e264fa122759dab623651caed629308c2
Dawit Yonas <dawityonas010@gmail.com>
2023-07-01 22:43:22 +0300

    Time: 1578 ms (38.24%), Space: 16.4 MB (21.32%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -0,0 +1,25 @@
+class Solution:
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+        cur = [0] * k
+        n = len(cookies)
+
+        def dfs(i, zero_count):
+            if n - i < zero_count:
+                return float('inf')
+            
+            if i == n:
+                return max(cur)
+            
+            answer = float('inf')
+            for j in range(k):
+                zero_count -= int(cur[j] == 0)
+                cur[j] += cookies[i]
+                
+                answer = min(answer, dfs(i + 1, zero_count))
+                
+                cur[j] -= cookies[i]
+                zero_count += int(cur[j] == 0)
+            
+            return answer
+        
+        return dfs(0, k)
