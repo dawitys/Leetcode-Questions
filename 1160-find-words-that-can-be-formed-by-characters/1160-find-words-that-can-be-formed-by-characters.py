04526c5f49a6efcda18304fc9c00523c4f3f5e4b
Dawit Yonas <dawityonas010@gmail.com>
2023-12-02 22:44:21 +0300

    Time: 150 ms (59.63%), Space: 16.9 MB (14.20%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
@@ -1,15 +1,15 @@
 class Solution:
     def countCharacters(self, words: List[str], chars: str) -> int:
         count = 0
+        char = Counter(chars)
         for word in words:
             can = True
-            char = Counter(chars)
             freq = Counter(word)
             for c in freq:
                 if freq[c] > char[c]:
                     can = False
                     break
-                char[c] -= freq[c]
+
             if can:
                 count += len(word)
                 
